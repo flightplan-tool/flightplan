d1e3ceb93bbe6a5edc911deb8e064b9d55afdea8
Jason Douglas <jasond@me.com>
2018-11-08 13:27:36 -0800

    Missing import

--- client/src/components/SearchForm.js
+++ client/src/components/SearchForm.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { inject, observer } from 'mobx-react'
+import { DateTime } from 'luxon'
 import moment from 'moment'
 import DatePicker from 'react-datepicker'
 import Select from 'react-select'
